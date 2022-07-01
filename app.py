ab644d04864cd299e0deadb4602d477c5240992f
vidya <vidya.deore@clairvoyantsoft.com>
2022-07-01 09:50:27 +0530

    Dockerized the application

--- app.py
+++ app.py
@@ -1,17 +1,24 @@
-import pandas as pd
+import os
+import sys
+from read import get_json_reader
+from write import load_db_table
+
+
+def process_table(BASE_DIR, conn, table_name):
+    json_reader = get_json_reader(BASE_DIR, table_name)
+    for df in json_reader:
+        load_db_table(df, conn, table_name, df.columns[0])
+
+
 def main():
-    fp = 'C:\\Users\\Vidya Deore\\Reasearch\\data\\retail_db_json\\order_items\\part-r-00000-6b83977e-3f20-404b-9b5f-29376ab1419e'
-    df = pd.read_json(fp, lines=True)
+    BASE_DIR = os.environ.get('BASE_DIR')
+    table_names = sys.argv[1].split(',')
 
-    #print(df.count())
-    #print(df.describe())
+    configs = dict(os.environ.items())
+    conn = f'postgresql://{configs["DB_USER"]}:{configs["DB_PASS"]}@{configs["DB_HOST"]}:{configs["DB_PORT"]}/{configs["DB_NAME"]}'
+    for table_name in table_names:
+        process_table(BASE_DIR, conn, table_name)
 
-    #print(df.columns)
-    #print(df.dtypes)
 
-    #print(df[['order_item_order_id', 'order_item_subtotal']])
-
-    print(df[df['order_item_order_id'] == 2])
-
-if __name__ == "__main__":
+if __name__ == '__main__':
     main()
